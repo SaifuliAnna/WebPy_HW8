65547873ef62a206c5c666a73dd05720a604ea2f
SaifuliAnna <saifulianna.it@gmail.com>
2022-08-30 16:48:44 +0300

    task_other

--- tasks_hw_8.sql
+++ tasks_hw_8.sql
@@ -1,38 +1,84 @@
 --1
 --
+SELECT id_students, id_subjects 
+FROM points
+WHERE (SELECT AVG(points) from points p)
+LIMIT 5;
 
+SELECT points, id_students, id_subjects 
+FROM points
+WHERE points >= (SELECT AVG(points) from points p)
+LIMIT 5;
 
 --2
 --
+SELECT points, id_students, id_subjects 
+FROM points
+WHERE points >= (SELECT AVG(points) from points p) AND id_subjects = 1006
+LIMIT 1;
+
+SELECT points, id_students, id_subjects 
+FROM points
+WHERE points >= (SELECT AVG(points) from points p) 
+LIMIT 1;
+
 SELECT AVG(points), id_students 
 FROM points 
-WHERE  id_subjects == 1006;
+WHERE  id_subjects = 1006;
 
 --3
 --
-SELECT AVG(points), id_groups  
-FROM points 
-WHERE  id_subjects == 2006;
+SELECT points, id_groups, id_subjects 
+FROM points
+WHERE points >= (SELECT AVG(points) from points p) 
+;
+
+SELECT AVG(points), id_groups, id_subjects
+FROM points
+WHERE id_groups = 6 AND id_subjects = 1006
+;
+
+SELECT AVG(points), id_professors, id_subjects
+FROM points
+WHERE id_professors = 606 AND id_subjects = 1006
+;
 
 --4
 --
+SELECT points, id_professors, id_subjects 
+FROM points
+WHERE points >= (SELECT AVG(points) from points p) 
+;
 
-
---5
---
 SELECT AVG(points), id_groups  
 FROM points 
 WHERE  id_subjects = 2006;
 
---6
+--5
 --
-SELECT students_name 
+SELECT groups_id, students_name 
 FROM students
 WHERE groups_id = 6;
 
+SELECT groups_id, students_name 
+FROM students
+;
+
+--6
+--
+SELECT points, id_groups, id_subjects  
+FROM points 
+WHERE  id_subjects = 2006;
+
+SELECT points, id_groups, id_subjects  
+FROM points 
+;
+
 --7
 --
-
+SELECT points, date_class, id_students, id_groups
+FROM points 
+WHERE date_class = '1992';
 
 --8
 --
@@ -40,26 +86,27 @@ SELECT DISTINCT id_subjects , id_students
 FROM points 
 WHERE id_students = 2;
 
+SELECT DISTINCT id_subjects , id_students 
+FROM points 
+;
+
 --9
 --
+SELECT DISTINCT id_subjects , id_students, id_professors
+FROM points p 
+;
 
-
---SELECT  id_subjects , id_students, id_professors, AVG(points) 
---FROM points 
---WHERE id_students = 2 
---AND id_subjects = 1007
---;
---
---SELECT  id_subjects , id_students, id_professors, AVG(points) 
---FROM points 
---WHERE id_students = 2;
---
---SELECT  id_subjects , id_students, id_professors, AVG(points) 
---FROM points 
---;
+SELECT DISTINCT points, id_subjects , id_students, id_professors
+FROM points p 
+WHERE points >= (SELECT AVG(points) from points p) 
+;
 
 --10
---SELECT AVG(points) FROM points;
+
+SELECT DISTINCT points, id_professors
+FROM points p 
+WHERE points >= (SELECT AVG(points) from points p) 
+;
 
 SELECT AVG(points), id_professors
 FROM points 
